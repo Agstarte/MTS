df66e0f2ca0dd0025376608579967015a2b75db3
Agstarte <aga1999.1@gmail.com>
2021-11-28 23:36:21 +0100

    final

--- classifiers.py
+++ classifiers.py
@@ -20,15 +20,15 @@ for k in k_values:
     for metric in metrics:
         clfs[f'K{k}_{metric}'] = KNeighborsClassifier(n_neighbors=k, metric=metric)
 
-
 dataset = np.genfromtxt("wisconsin.csv", delimiter=",")
 X = dataset[1:, :-1]
 y = dataset[1:, -1].astype(int)
+
 ranking = r_regression(X, y, center=True)
 
-for i in range(1, len(ranking)+1):
+for i in range(1, len(ranking) + 1):
 
-    print(f'='*26 +
+    print(f'=' * 26 +
           f'\n{i} feature(s):')
 
     # getting ids of i columns with highest rankings
@@ -42,41 +42,23 @@ for i in range(1, len(ranking)+1):
     y = new_dataset[1:, -1].astype(int)
 
     # cross validation if more than one feature
-    if i > 1:
-        if i < 5:
-            # if there are less than 5 features, n_splits are equal to i
-            n_splits = i
-        else:
-            n_splits = 5
-        n_repeats = 2
-        rskf = RepeatedStratifiedKFold(
-            n_splits=n_splits, n_repeats=n_repeats, random_state=123)
 
-        scores = np.zeros((len(clfs), n_splits * n_repeats))
+    n_splits = 2
+    n_repeats = 5
+    rskf = RepeatedStratifiedKFold(
+        n_splits=n_splits, n_repeats=n_repeats, random_state=123)
 
-        for fold_id, (train, test) in enumerate(rskf.split(X, y)):
-            for clf_id, clf_name in enumerate(clfs):
-                clf = clone(clfs[clf_name])
-                clf.fit(X[train], y[train])
-                y_pred = clf.predict(X[test])
-                scores[clf_id, fold_id] = accuracy_score(y[test], y_pred)
-    else:
-        # if one feature then we just split dataset
-        scores = np.zeros((len(clfs), 1))
+    scores = np.zeros((len(clfs), n_splits * n_repeats))
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y,
-            test_size=.30,
-            random_state=123
-        )
+    for fold_id, (train, test) in enumerate(rskf.split(X, y)):
         for clf_id, clf_name in enumerate(clfs):
             clf = clone(clfs[clf_name])
-            clf.fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
-            scores[clf_id, 0] = accuracy_score(y_test, y_pred)
+            clf.fit(X[train], y[train])
+            y_pred = clf.predict(X[test])
+            scores[clf_id, fold_id] = accuracy_score(y[test], y_pred)
 
     mean = np.mean(scores, axis=1)  # arithmetic mean of accuracies
-    std = np.std(scores, axis=1)    # standard deviation
+    std = np.std(scores, axis=1)  # standard deviation
 
     for clf_id, clf_name in enumerate(clfs):
         print("%s: %.3f (%.2f)" % (clf_name, mean[clf_id], std[clf_id]))
@@ -84,4 +66,3 @@ for i in range(1, len(ranking)+1):
     if not os.path.isdir('results'):
         os.mkdir('results')
     np.save(os.path.join('results', f'results_{i}'), scores)
-
